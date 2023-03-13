f809fe11e3088394746e72f204939bd3861d4b17
qq862 <qq86280630qq@163.com>
2023-03-13 15:32:29 +0800

    Setting up GitHub Classroom Feedback

---
+++ src/main/java/prog2/exercise5/flight/booking/system/Main.java
@@ -0,0 +1,16 @@
+package prog2.exercise5.flight.booking.system;
+
+
+
+
+public class Main
+{
+    public static void main(String[] args )
+    {
+        int size=3;
+        FlightBooking fb = new FlightBooking(size);
+
+
+
+    }
+}
